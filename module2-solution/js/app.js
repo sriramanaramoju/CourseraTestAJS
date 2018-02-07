81411a9eed0df5e771a949734784430a77ba68ac
Ramoju <sriramana.ramoju@honeywell.com>
2018-02-07 10:49:22 +0530

    MyFirstAssignment

--- module2-solution/js/app.js
+++ module2-solution/js/app.js
@@ -10,38 +10,33 @@ function ToBuyController(ShoppingListService) {
   var buyctrl = this;
   var length = 4;
   var Itemstobuy = [{itemQuantity: 10, itemName: "Cookies"},{itemQuantity: 10, itemName: "Choclates"},{itemQuantity: 10, itemName: "Icecreams"},{itemQuantity: 10, itemName: "Books"},{itemQuantity: 10, itemName: "Pens"}];
-  console.log(buyctrl[1]);
   for (var i=0; i<5; i++)
   {
-    console.log("inside");
-    console.log(i);
-  buyctrl.addItem = (function () { ShoppingListService.addItem(Itemstobuy[i].itemName, Itemstobuy[i].itemQuantity); console.log(Itemstobuy[i]) })();
+    buyctrl.addItem = (function () { ShoppingListService.addItem(Itemstobuy[i].itemName, Itemstobuy[i].itemQuantity); console.log(Itemstobuy[i]) })();
   }
-  buyctrl.items = ShoppingListService.getItems();
-  buyctrl.removeItem = function (itemIndex) {
-    ShoppingListService.removeItem(itemIndex);
-    ShoppingListService.moveitem(itemIndex);
-    console.log(buyctrl.items.length);
+    buyctrl.items = ShoppingListService.getItems();
+    buyctrl.removeItem = function (itemIndex) {
+    ShoppingListService.removeItem(itemIndex, "frombuyctrl");
     length =buyctrl.items.length;
-    console.log(length);
+
   };
 }
 AlreadyBoughtController.$inject = ['ShoppingListService'];
 function AlreadyBoughtController(ShoppingListService) {
   var boughtctrl = this;
-
-  //boughtctrl.items = ShoppingListService.getItems();
-
+  boughtctrl.moveditems = ShoppingListService.getmovedItems();
   boughtctrl.removeItem = function (itemIndex) {
-    ShoppingListService.removeItem(itemIndex);
+  ShoppingListService.removeItem(itemIndex,"fromboughtctrl");
+  length =boughtctrl.moveditems.length;
   };
 }
+
 function ShoppingListService() {
   var service = this;
-
   // List of shopping items
   var items = [];
   var moveditems = [];
+  var i=0;
   service.addItem = function (itemName, quantity) {
     console.log("ram");
     var item = {
@@ -52,18 +47,26 @@ function ShoppingListService() {
     console.log(items[4]);
   };
 
-  service.removeItem = function (itemIdex) {
-    items.splice(itemIdex, 1);
+  service.removeItem = function (itemIdex,controller) {
+    if(controller == "frombuyctrl" )
+    {
+      moveditems.push(items[itemIdex]);
+      items.splice(itemIdex, 1);
+      i++;
+    }
+    if(controller == "fromboughtctrl")
+    {
+      service.addItem(moveditems[itemIdex].name, moveditems[itemIdex].quantity);
+      moveditems.splice(itemIdex,1);
+    }
   };
-
-  service.moveitem = function (itemindex) {
-    moveditems.push(items[itemindex]);
-    console.log(moveditems[0]);
-        console.log(moveditems[1]);
-  }
 
   service.getItems = function () {
     return items;
+  };
+
+  service.getmovedItems = function () {
+    return moveditems;
   };
 }
 })();
